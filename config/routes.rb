5b10dc89efb66e88b38cc7b9f28c34d013ac402d
redhendery <redhendery@gmail.com>
2019-03-05 14:22:13 -0800

    add basic login and logout methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
 
 # Static Routes
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/sponsors', to: 'static_pages#sponsors'
   get '/signup', to: 'users#new'
-  post '/signup',  to: 'users#create'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users
 end
