c03c58de0cf4df583d5acb8da2808a9930f76b52
redhendery <redhendery@gmail.com>
2019-04-05 11:55:31 -0700

    update home page for members, other style changes to be mobile friendly

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
+    @upcoming = Schedule.where(date: next_week)
+    @previous = Schedule.where(date: last_week)
   end
 
   def about
-  end
-
-  def contact
   end
 
   def membership
--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -4,4 +4,8 @@ module SchedulesHelper
     Date.current..(Date.current + 7)
   end
 
+  def last_week
+    (Date.current - 7)..Date.current
+  end
+
 end
